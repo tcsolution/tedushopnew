4ff1c6262d053e5bab7e33370b68353da87942b8
bikool <chienhtbmt@gmail.com>
2018-09-04 14:40:26 +0700

    commit migration

---
+++ TeduShop.Data/Reponsitories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+    }
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+        
+    }
+}
